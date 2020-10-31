0de22084731338956f86100992ab54ec8c9b8dd7
nandoibba <nandoibba@gmail.com>
2020-10-31 22:50:43 +0100

    Ajout commentaire

--- controller/maincontroller.php
+++ controller/maincontroller.php
@@ -8,7 +8,7 @@ $userpass=htmlspecialchars($_POST['userpass']);
 $donneesco=selectuser($userid,$userpass);
 $donnees = $donneesco->fetch();
 
-if ($userid == $donnees['identifiant'] AND $userpass == $donnees['pass'])
+if ($userid == $donnees['identifiant'] AND $userpass == $donnees['pass'])//on verifie les informations de connexion et on dirige vers la page eleve, prof ou admin
 {
     $_SESSION['userid']=$userid;
     $_SESSION['userpass']=$userpass;
